5b6a2ec1fce3cca8061dcfd50ab4827dc1eadd2a
TingYi <lavender55260@gmail.com>
2022-05-06 14:24:04 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/sa/RegisterActivity.java
@@ -0,0 +1,34 @@
+package com.example.sa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+    private Button btnBackMain,btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        btnBackMain=findViewById(R.id.btnBackMain);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnBackMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
