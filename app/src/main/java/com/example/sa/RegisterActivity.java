f464bc8a53d5c3e076a9c24ec10a5309c875cb47
Min <wyes91622@gmail.com>
2022-05-08 02:55:22 +0800

    Add OkHttp

--- app/src/main/java/com/example/sa/RegisterActivity.java
+++ app/src/main/java/com/example/sa/RegisterActivity.java
@@ -3,12 +3,26 @@ package com.example.sa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class RegisterActivity extends AppCompatActivity {
     private Button btnBackMain,btnLogin;
+    OkHttpClient client = new OkHttpClient();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +40,37 @@ public class RegisterActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                class TestTask extends AsyncTask<Void, Void, Void> {
+                    @Override
+                    protected Void doInBackground(Void... voids) {
+                        JSONObject jsonObject = new JSONObject();
+                        try {
+                            jsonObject.put("email", "123@gmail.com");
+                            jsonObject.put("lastName", "test");
+                            jsonObject.put("name", "test1");
+                            jsonObject.put("password", "123456");
+                            jsonObject.put("userName", "test1");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        MediaType mediaType  = MediaType.get("application/json; charset=utf-8");
+                        RequestBody body = RequestBody.create(jsonObject.toString(), mediaType);
+                        Request request = new Request.Builder()
+                                .url("http://140.125.207.230:8080/api/register")
+                                .post(body)
+                                .build();
+
+                        try (Response response = client.newCall(request).execute()) {
+                            System.out.println(response.body().string());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        return null;
+                    }
+                }
+                new TestTask().execute();
+
                 Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
                 startActivity(intent);
             }
