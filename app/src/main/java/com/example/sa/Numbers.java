5476d7315ad58ab8b5dc28d0e6079ac8d9d4302d
kent <kent900919@gmail.com>
2022-10-23 21:23:15 +0800

    HTTP Status Initial Commit

---
+++ app/src/main/java/com/example/sa/Numbers.java
@@ -0,0 +1,13 @@
+package com.example.sa;
+
+public class Numbers {
+    private int httpnum;
+
+    public Numbers(int httpN){
+        httpnum = httpN;
+
+    }
+
+    public int gethttpN1(){return httpnum;}
+
+}
---
+++ app/src/main/java/com/example/sa/http_is_404.java
@@ -0,0 +1,24 @@
+package com.example.sa;
+
+public class http_is_404 implements httpNum{
+
+    private httpNum nextInHttp;
+
+    @Override
+    public void setNexthttp(httpNum nexthttp) {
+
+        this.nextInHttp = nexthttp;
+
+    }
+
+    @Override
+    public boolean httpstate(Numbers  request) {
+        if (request.gethttpN1() == 404){
+            return false;
+        }else {
+            nextInHttp.httpstate(request);
+        }
+
+        return false;
+    }
+}
