717069a3014d8ae364d3d2e6f258efc5f4a19bd4
kent <kent900919@gmail.com>
2022-12-06 18:19:38 +0800

    Merge pull request #14 from fan9704/feature_ProxyBankAcct
    Feature proxy bank acct

---
+++ app/src/main/java/com/example/sa/command/ConcreteCommand.java
@@ -0,0 +1,14 @@
+package com.example.sa.command;
+
+public class ConcreteCommand extends command {
+
+    @Override
+    public void excute() {
+
+    }
+
+    @Override
+    public void undo() {
+
+    }
+}
