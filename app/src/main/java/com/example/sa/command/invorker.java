04f7f1536f56992b08f8a9cbca9064379b8bb4ef
linjiaming <kent900919@gmail.com>
2022-12-11 22:00:34 +0800

    command add moneystate2

--- app/src/main/java/com/example/sa/command/invorker.java
+++ app/src/main/java/com/example/sa/command/invorker.java
@@ -1,4 +1,23 @@
 package com.example.sa.command;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class invorker {
+    private List<Command> commandList = new ArrayList<Command>();
+    private List<Command> commandList1 = new ArrayList<Command>();
+
+    public void addCommend(Command Command) {
+        commandList.add(Command);
+    }
+    public void execute() {
+        for(Command command : commandList) {
+            command.execute();
+        }
+    }
+    public void unexecute(){
+        for (Command command : commandList1){
+            command.unexecute();
+        }
+    }
 }
